d18a255ff2f07c73f9dc22e039b6f8614f38b6d7
Frank <frank@Franks-MacBook-Air.local>
2024-08-11 13:02:38 -0400

    third commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const launchPrompt = () => {
+    let dynamicText = prompt("Whats your name ?");
+    let screen = document.querySelector(".screen");
+ screen.innerHTML = "Welcome " + dynamicText +"! 😁";
+};
+
+
+
+const clickText = document.getElementById('clickText');
+
+clickText.addEventListener('click', () => {
+    clickText.style.color = "blue";
+
+});
+
+
